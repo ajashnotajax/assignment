22c505f4b642fbef0f7bb09c8578c4cc8398b5ae
ajashnotajax <sherawatajay4@gmail.com>
2020-12-21 22:47:42 +0530

    Updates

---
+++ precache-manifest.3acbd34df47155cfcb913690dc974ac7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99377bf8f5221ea74a279b122899166d",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "840d63710ff827af9b54",
+    "url": "/assignment/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "c9a24f3c6fb92a75ac58",
+    "url": "/assignment/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "840d63710ff827af9b54",
+    "url": "/assignment/static/js/2.cfb54add.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/assignment/static/js/2.cfb54add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a24f3c6fb92a75ac58",
+    "url": "/assignment/static/js/main.4a8a40c0.chunk.js"
+  },
+  {
+    "revision": "e3a2b13b3f6c54246d5d",
+    "url": "/assignment/static/js/runtime-main.93b124ca.js"
+  },
+  {
+    "revision": "fe220c29d43df716685defa0d0c7c7d7",
+    "url": "/assignment/static/media/Photo.fe220c29.jpg"
+  }
+]);
